8c993b7ca216a2ed9f2a64db713d9f2381f156f3
mugala <mugalalevi21@gmail.com>
2018-02-23 18:03:33 +0300

    Adding JS code this commit will add the js codeX

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,20 @@
 // business logic
-  var leapYear = function(year) {
+var numbersToBePingponged = [];
 
-  };
+function pingPong (number){
+    for (var index = 1; index <= number; index+=1){
+}
+
 
   // user interface logic
   $(document).ready(function() {
-    $("form#ping-pong").submit(function(event) {
+    $("form#ping-pong").submit(function() {
       event.preventDefault();
-      var year = parseInt($("input#num").val());
+      var number = parseInt($("input#num").val());
       var result = leapYear(year);
 
-      $(".year").text(year);
+      pingPong(number);
 
-      if (!result) {                 // same as writing if (result === false)
-        $(".not").text("");
-      } else {
-        $(".not").text("");
-      }
 
       $("#result").show();
     });
