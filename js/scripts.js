8d796617a7099ca749c52ff6e702d0855547641a
mugala <mugalalevi21@gmail.com>
2018-02-24 18:11:48 +0300

    Business logic. This commit will ensure that the back end code for the ping pong game works.

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
    });
 });
 
-          //using switch statements
+/*          //using switch statements
  var pingPongNum = [];
 
 function ping(number){
@@ -36,4 +36,26 @@ function ping(number){
 }
     }
 }
-} 
+} */
+//back-end code
+var pingPongNum = [];
+
+function ping(number){
+    for (var i = 1; i <= number; i+=1){
+// Start with 15 so that when it goes through the loop assissing each number it eveluate 15 and returns Ping-Pong instead of just Ping and/or Pong since 15 is divisible by
+//both 3 and 5.
+        if(i % 15 === 0){
+            pingPongNum.push("Ping-Pong");
+        }
+        else if(i % 5 === 0){
+            pingPongNum.push("Pong");
+        }
+        else if(i % 3 === 0){
+            pingPongNum.push("Ping");
+        }
+        else{
+          //in the event that the number is not divisible by 3, 5 or 15 then return the number itself.
+            pingPongNum.push(i);
+        }
+    }
+}
