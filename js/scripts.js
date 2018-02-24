bc7c1be20536f9cbf924c1b3ce8934e90d279d86
mugala <mugalalevi21@gmail.com>
2018-02-24 18:27:52 +0300

    Indentation. This commit will ensure that the index.html, styles.css and the scripts.js are indented correctly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 //front-end code
-$(document).ready(function(){
-   $("form#ping-pong").submit(function(){
-      event.preventDefault();
-      var number = parseInt($("input#number").val());
+$(document).ready(function() {
+  $("form#ping-pong").submit(function() {
+    event.preventDefault();
+    var number = parseInt($("input#number").val());
 
-      ping(number);
+    ping(number);
 
-      pingPongNum.forEach(function(number){
-         $("#output").append('<li>' + number + '</li>');
-      });
-   });
+    pingPongNum.forEach(function(number) {
+      $("#output").append('<li>' + number + '</li>');
+    });
+  });
 });
 
 /*          //using switch statements
@@ -40,22 +40,19 @@ function ping(number){
 //back-end code
 var pingPongNum = [];
 
-function ping(number){
-    for (var i = 1; i <= number; i+=1){
-// Start with 15 so that when it goes through the loop assissing each number it eveluate 15 and returns Ping-Pong instead of just Ping and/or Pong since 15 is divisible by
-//both 3 and 5.
-        if(i % 15 === 0){
-            pingPongNum.push("Ping-Pong");
-        }
-        else if(i % 5 === 0){
-            pingPongNum.push("Pong");
-        }
-        else if(i % 3 === 0){
-            pingPongNum.push("Ping");
-        }
-        else{
-          //in the event that the number is not divisible by 3, 5 or 15 then return the number itself.
-            pingPongNum.push(i);
-        }
+function ping(number) {
+  for (var i = 1; i <= number; i += 1) {
+    // Start with 15 so that when it goes through the loop assissing each number it eveluate 15 and returns Ping-Pong instead of just Ping and/or Pong since 15 is divisible by
+    //both 3 and 5.
+    if (i % 15 === 0) {
+      pingPongNum.push("Ping-Pong");
+    } else if (i % 5 === 0) {
+      pingPongNum.push("Pong");
+    } else if (i % 3 === 0) {
+      pingPongNum.push("Ping");
+    } else {
+      //in the event that the number is not divisible by 3, 5 or 15 then return the number itself.
+      pingPongNum.push(i);
     }
+  }
 }
