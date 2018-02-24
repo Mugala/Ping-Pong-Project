fbe9d998723bf8cfe7738cd02e55f7b3bed2c7d1
mugala <mugalalevi21@gmail.com>
2018-02-24 18:10:01 +0300

    Building the back end using. Attempting using the switch statement and the for loop to build the business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,29 @@ $(document).ready(function(){
       });
    });
 });
+
+          //using switch statements
+ var pingPongNum = [];
+
+function ping(number){
+    for (var i = 1; i <= number; i++){
+            switch (number){
+        case i % 15 === 0:{
+           pingPongNum.push("Ping-Pong");
+         }
+          break;
+        case i % 5 === 0:{
+           pingPongNum.push("Pong");
+         }
+          break;
+        case i %  3 === 0:{
+           pingPongNum("Ping");
+         }
+          break;
+        default:{
+          pingPongNum.push(i);
+         break;
+}
+    }
+}
+} 
